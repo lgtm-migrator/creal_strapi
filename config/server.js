4151fa5f5f6dceb014b3e863fc2e7ec87e415acd
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-02-09 19:20:02 +0100

    feat(server): read secret file environment variable

--- config/server.js
+++ config/server.js
@@ -1,9 +1,29 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+const fs = require('fs');
+
+module.exports = ({ env }) => {
+  let jwtSecret;
+
+  if (process.env['ADMIN_JWT_SECRET_FILE']) {
+    try {
+      jwtSecret = fs
+        .readFileSync(process.env['ADMIN_JWT_SECRET_FILE'], {
+          encoding: 'utf8',
+        })
+        .trim();
+      process.env['ADMIN_JWT_SECRET'] = jwtSecret;
+    } catch (err) {
+      console.log('Failed to read ADMIN_JWT_SECRET_FILE!');
+      console.log(err);
+    }
+  }
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET'),
+      },
     },
-  },
-});
+  }
+};
